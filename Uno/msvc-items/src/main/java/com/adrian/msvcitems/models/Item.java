dcae0672d72b724f7e38caa6a33edd60d4766966
adrianprzcb <adrianpc4444@gmail.com>
2024-11-30 16:07:47 +0100

    s

--- Uno/msvc-items/src/main/java/com/adrian/msvcitems/models/Item.java
+++ Uno/msvc-items/src/main/java/com/adrian/msvcitems/models/Item.java
@@ -1,10 +1,7 @@
 package com.adrian.msvcitems.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "items")
+
 public class Item {
 
     private Product product;
