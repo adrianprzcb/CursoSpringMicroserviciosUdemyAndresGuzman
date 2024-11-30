22f337c66f1757d5f571ccafbc46c04f28d9ba03
adrianprzcb <adrianpc4444@gmail.com>
2024-11-30 14:51:38 +0100

    s

--- Uno/msvc-products/src/main/java/com/adrian/msvcproducts/services/implementations/ProductServiceImpl.java
+++ Uno/msvc-products/src/main/java/com/adrian/msvcproducts/services/implementations/ProductServiceImpl.java
@@ -3,14 +3,19 @@ package com.adrian.msvcproducts.services.implementations;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.adrian.msvcproducts.entities.Product;
+import com.adrian.msvcproducts.repositories.ProductRepository;
 import com.adrian.msvcproducts.services.ProductService;
 
 @Service
 public class ProductServiceImpl implements ProductService{
+
+    @Autowired
+    private ProductRepository repository;
 
     @Override
     @Transactional(readOnly = true)
