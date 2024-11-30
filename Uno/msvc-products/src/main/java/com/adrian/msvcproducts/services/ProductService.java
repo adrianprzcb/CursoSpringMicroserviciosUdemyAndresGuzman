2765fd589a60a300602422614cde8dcf3fed06ae
adrianprzcb <adrianpc4444@gmail.com>
2024-11-30 14:48:21 +0100

    s

--- Uno/msvc-products/src/main/java/com/adrian/msvcproducts/services/ProductService.java
+++ Uno/msvc-products/src/main/java/com/adrian/msvcproducts/services/ProductService.java
@@ -1,10 +1,16 @@
 package com.adrian.msvcproducts.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.adrian.msvcproducts.entities.Product;
 
 @Service
 public interface ProductService {
 
-    
+    List<Product> findAll();
+    Optional<?> findById();
+
 
 }
