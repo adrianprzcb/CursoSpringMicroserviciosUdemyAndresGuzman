29d50e59bf5d8654cdbb8677c008bb49559c571f
adrianprzcb <adrianpc4444@gmail.com>
2024-11-30 16:30:51 +0100

    s

---
+++ Uno/msvc-items/src/main/java/com/adrian/msvcitems/services/ItemServiceFeign.java
@@ -0,0 +1,25 @@
+package com.adrian.msvcitems.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.adrian.msvcitems.models.Item;
+
+@Service
+public class ItemServiceFeign implements ItemService{
+
+    @Override
+    public List<Item> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+    
+}
