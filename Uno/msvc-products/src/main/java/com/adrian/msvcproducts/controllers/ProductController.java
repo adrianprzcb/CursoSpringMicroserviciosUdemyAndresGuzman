c291bff7984d697913ca45709f5ed66cc4e4e0f8
adrianprzcb <adrianpc4444@gmail.com>
2024-11-30 14:59:04 +0100

    s

--- Uno/msvc-products/src/main/java/com/adrian/msvcproducts/controllers/ProductController.java
+++ Uno/msvc-products/src/main/java/com/adrian/msvcproducts/controllers/ProductController.java
@@ -1,8 +1,15 @@
 package com.adrian.msvcproducts.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.adrian.msvcproducts.entities.Product;
 import com.adrian.msvcproducts.services.ProductService;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class ProductController {
@@ -14,6 +21,17 @@ public class ProductController {
         this.productService = productService;
     }
 
+
+    @GetMapping
+    public List<Product> list() {
+        return this.productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product details(@PathVariable Long id) {
+        Optional<Product> productOpt = 
+    }
+    
 
 
 
