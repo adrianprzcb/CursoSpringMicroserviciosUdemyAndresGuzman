17ca93e829530db327c58cb9f2af7b2a9871a44a
adrianprzcb <adrianpc4444@gmail.com>
2024-11-30 14:32:32 +0100

    s

--- Uno/msvc-products/src/main/java/com/adrian/msvcproducts/entities/Product.java
+++ Uno/msvc-products/src/main/java/com/adrian/msvcproducts/entities/Product.java
@@ -1,5 +1,8 @@
 package com.adrian.msvcproducts.entities;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,5 +19,55 @@ public class Product {
     private String name;
     private Double price;
 
+    @Column(name = "create_at")
+    private LocalDate createAt;
+
+
+    public Product(){
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Double getPrice() {
+        return price;
+    }
+
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+
+    public LocalDate getCreateAt() {
+        return createAt;
+    }
+
+
+    public void setCreateAt(LocalDate createAt) {
+        this.createAt = createAt;
+    }
+
     
+
+
 }
