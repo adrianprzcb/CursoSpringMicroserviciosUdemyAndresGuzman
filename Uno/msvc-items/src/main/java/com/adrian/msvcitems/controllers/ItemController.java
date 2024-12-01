61c411d589aed285f1530dca71f3548248042ff8
adrianprzcb <adrianpc4444@gmail.com>
2024-12-01 16:41:53 +0100

    s

--- Uno/msvc-items/src/main/java/com/adrian/msvcitems/controllers/ItemController.java
+++ Uno/msvc-items/src/main/java/com/adrian/msvcitems/controllers/ItemController.java
@@ -1,9 +1,15 @@
 package com.adrian.msvcitems.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.adrian.msvcitems.models.Item;
 import com.adrian.msvcitems.services.ItemService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class ItemController {
@@ -15,6 +21,10 @@ public class ItemController {
     }
 
 
+    @GetMapping
+    public List<Item> list() {
+       return itemService.findAll();
+    }
     
 
 }
