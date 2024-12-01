8b561806c31cf3e5faef41621bca9938194ac2ab
adrianprzcb <adrianpc4444@gmail.com>
2024-12-01 23:09:23 +0100

    s

--- Uno/msvc-products/src/main/java/com/adrian/msvcproducts/services/implementations/ProductServiceImpl.java
+++ Uno/msvc-products/src/main/java/com/adrian/msvcproducts/services/implementations/ProductServiceImpl.java
@@ -3,6 +3,7 @@ package com.adrian.msvcproducts.services.implementations;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,15 +21,19 @@ public class ProductServiceImpl implements ProductService{
 
    final private ProductRepository productRepository;
 
+   final private Environment environment;
 
-   public ProductServiceImpl(ProductRepository productRepository){
+
+   public ProductServiceImpl(ProductRepository productRepository, Environment environment){
     this.productRepository = productRepository;
+    this.environment = environment;
    }
 
     @Override
     @Transactional(readOnly = true)
     public List<Product> findAll() {
-       return (List<Product>) productRepository.findAll();
+       return ((List<Product>) productRepository.findAll()).stream().map(product ->
+       return product);
     }
 
     @Override
