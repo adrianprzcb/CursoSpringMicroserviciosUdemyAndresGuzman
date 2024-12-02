a3f8e04bdf9b1c028c3c9a8a4bf8f9f6f1901869
adrianprzcb <adrianpc4444@gmail.com>
2024-12-02 11:02:39 +0100

    s

--- Uno/msvc-items/src/main/java/com/adrian/msvcitems/WebClientConfig.java
+++ Uno/msvc-items/src/main/java/com/adrian/msvcitems/WebClientConfig.java
@@ -1,11 +1,13 @@
 package com.adrian.msvcitems;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
+    @Bean
     WebClient.Builder webClient() {
         return WebClient.builder();
     }
