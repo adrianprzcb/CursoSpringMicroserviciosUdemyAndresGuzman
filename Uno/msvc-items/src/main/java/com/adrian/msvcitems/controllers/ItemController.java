73614be7ff6f31ba06bc94227b36819a2a34271c
adrianprzcb <adrianpc4444@gmail.com>
2024-12-02 16:58:21 +0100

    s

--- Uno/msvc-items/src/main/java/com/adrian/msvcitems/controllers/ItemController.java
+++ Uno/msvc-items/src/main/java/com/adrian/msvcitems/controllers/ItemController.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class ItemController {
 
     private final ItemService itemService;
 
-    public ItemController(ItemService itemService){
+    public ItemController(@Qualifier("itemServiceWebClient") ItemService itemService){
         this.itemService = itemService;
     }
 
