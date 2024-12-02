d88f2c59896d749f514a00027087429e2fccc350
adrianprzcb <adrianpc4444@gmail.com>
2024-12-02 09:54:23 +0100

    s

--- Uno/msvc-items/src/main/java/com/adrian/msvcitems/clients/ProductFeignClient.java
+++ Uno/msvc-items/src/main/java/com/adrian/msvcitems/clients/ProductFeignClient.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.adrian.msvcitems.models.Product;
-
-@FeignClient(url = "localhost:8080", name = "msvc-products")
+//@FeignClient(url = "localhost:8080", name = "msvc-products")
+@FeignClient( name = "msvc-products")
 public interface ProductFeignClient {
 
     @GetMapping()
