a35bdf6576a8a1b50b739fbbf3cbae6220101bd1
adrianprzcb <adrianpc4444@gmail.com>
2024-12-06 22:30:17 +0100

    s

--- SpringCloud/msvc-gateway-server/src/main/java/com/adrian/msvc_gateway_server/filters/SampleGlobalFilter.java
+++ SpringCloud/msvc-gateway-server/src/main/java/com/adrian/msvc_gateway_server/filters/SampleGlobalFilter.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -12,7 +13,7 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class SampleGlobalFilter implements GlobalFilter{
+public class SampleGlobalFilter implements GlobalFilter, Ordered{
 
     private Logger logger = LoggerFactory.getLogger(SampleGlobalFilter.class);
 
@@ -21,9 +22,16 @@ public class SampleGlobalFilter implements GlobalFilter{
         
         logger.info("Ejecutando filtro antes del request");
 
+
+        exchange.getRequest().mutate().headers(h -> h.add("token", "headerValue"));
+
+
+
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
             logger.info("ejecutando filtro POST");
+            logger.info("token:" + exchange.getRequest().getHeaders().get("token"));
 
+            
 
             exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "red").build());
             exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
@@ -31,6 +39,13 @@ public class SampleGlobalFilter implements GlobalFilter{
         
     }
 
-    
+    @Override
+    public int getOrder() {
+       return 100;
+    }
+
+  
+
 
 }
+
