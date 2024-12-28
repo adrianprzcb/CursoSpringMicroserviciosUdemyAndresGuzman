1c0ec7a2aac111f1c2b1694221d14c1081376c77
adrianprzcb <adrianpc4444@gmail.com>
2024-12-28 17:43:52 +0100

    s

---
+++ commons/libs-mcvc-commons/src/main/java/com/andres/libs/msvc/commons/libs_mcvc_commons/LibsMcvcCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.andres.libs.msvc.commons.libs_mcvc_commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibsMcvcCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibsMcvcCommonsApplication.class, args);
+	}
+
+}
