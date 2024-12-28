cfa50a83928b88b9430c87e2dfdd8b7465f32a85
adrianprzcb <adrianpc4444@gmail.com>
2024-12-28 18:12:49 +0100

    S

--- commons/libs-mcvc-commons/src/main/java/com/andres/libs/msvc/commons/libs_mcvc_commons/LibsMcvcCommonsApplication.java
+++ commons/libs-mcvc-commons/src/main/java/com/andres/libs/msvc/commons/libs_mcvc_commons/LibsMcvcCommonsApplication.java
@@ -1,13 +1,15 @@
 package com.andres.libs.msvc.commons.libs_mcvc_commons;
 
+import org.hibernate.annotations.processing.Exclude;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class LibsMcvcCommonsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibsMcvcCommonsApplication.class, args);
-	}
+
 
 }
