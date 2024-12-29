adf39b79b6609d94b023b4e8e280344688fead51
adrianprzcb <adrianpc4444@gmail.com>
2024-12-29 01:27:59 +0100

    s

--- Uno/msvc-items/src/main/java/com/andres/springcloud/msvc/items/MsvcItemsApplication.java
+++ Uno/msvc-items/src/main/java/com/andres/springcloud/msvc/items/MsvcItemsApplication.java
@@ -1,9 +1,13 @@
 package com.andres.springcloud.msvc.items;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableFeignClients
 @SpringBootApplication
 public class MsvcItemsApplication {
