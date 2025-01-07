f656697b0c3584764b60efc4c44a382fba552b4a
adrianprzcb <adrianpc4444@gmail.com>
2025-01-07 17:33:28 +0100

    s

--- SpringCloud/msvc-oauth/src/main/java/com/andres/springcloud/msvc/oauth/services/UsersService.java
+++ SpringCloud/msvc-oauth/src/main/java/com/andres/springcloud/msvc/oauth/services/UsersService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.GrantedAuthority;
@@ -21,12 +23,16 @@ import com.andres.springcloud.msvc.oauth.models.User;
 @Service
 public class UsersService implements UserDetailsService {
 
+        private final Logger logger = LoggerFactory.getLogger(UsersService.class);
+
+
     @Autowired
     private WebClient.Builder client;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
+        logger.info("Ingresando al proceso de login UsersService::loadUserByUsername con {}", username);
         Map<String, String> params = new HashMap<>();
         params.put("username", username);
 
@@ -41,11 +47,18 @@ public class UsersService implements UserDetailsService {
                     .stream()
                     .map(role -> new SimpleGrantedAuthority(role.getName()))
                     .collect(Collectors.toList());
-            return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+
+            org.springframework.security.core.userdetails.User userdetails = new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
                     user.isEnabled(), true, true, true, roles);
-        } catch (WebClientResponseException e) {
+
+                    logger.info("Se ha realizado el login con exito con {}", user);
+
+                    return userdetails;
+                } catch (WebClientResponseException e) {
+                    String error = "Error en el login, no existe el users '" + username + "' en el sistema";
+                    logger.error(error);
             throw new UsernameNotFoundException(
-                    "Error en el login, no existe el users '" + username + "' en el sistema");
+                  error);
         }
 
     }
--- SpringCloud/msvc-users/src/main/java/com/andres/springcloud/msvc/users/controllers/UserController.java
+++ SpringCloud/msvc-users/src/main/java/com/andres/springcloud/msvc/users/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.andres.springcloud.msvc.users.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,38 +13,47 @@ import com.andres.springcloud.msvc.users.services.IUserService;
 @RestController
 public class UserController {
 
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+
     @Autowired
     private IUserService userService;
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
+        logger.info("UserController::createUser : creando {}", user);
         return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable Long id) {
+        logger.info("UserController::updateUser : actualizando {}", user);
         return userService.update(user, id).map(userUpdated -> ResponseEntity.status(HttpStatus.CREATED).body(userUpdated))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        logger.info("UserController::getUserById : getUserById {}", id);
         return userService.findById(id).map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping("/username/{username}")
     public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+        logger.info("UserController::getUserByUsername  LOGI con {}", username);
         return userService.findByUsername(username).map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping
     public ResponseEntity<Iterable<User>> getAllUsers() {
+        logger.info("UserController::getAllUsers");
         return ResponseEntity.ok(userService.findAll());
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        logger.info("UserController::deleteUser : deleteUser {}", id);
         userService.delete(id);
         return ResponseEntity.noContent().build();
     }
